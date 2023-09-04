b7d5cc42117ed3c13393b2b75678b146f9b58115
ahmedabdrabou110 <a.abdrabou1102000@icloud.com>
2023-09-04 15:03:40 +0300

    create structute and design of left side and post modal element

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import UserPhoto from "../../assets/user.svg";
+import { styled } from "styled-components";
+import PostModal from "./PostModal";
 const Main = () => {
   const user = useSelector((state) => state.userState.user);
   const [showModal, setShowModal] = useState(false);
@@ -20,8 +22,92 @@ const Main = () => {
           <button onClick={handleClick}>Start a post</button>
         </div>
       </ShareBox>
+      <Content></Content>
+      <PostModal showModal={showModal} handleClick={handleClick} />
     </Container>
   );
 };
+
+const Container = styled.div`
+  grid-area: main;
+`;
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 1px rgb(0 0 0 / 20%);
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 500;
+      cursor: pointer;
+      transition: background 0.3s ease;
+      border-radius: 5px;
+      &:hover {
+        background: rgba(0, 0, 0, 0.08);
+      }
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 8px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        background: white;
+        color: rgba(0, 0, 0, 0.7);
+        font-weight: 500;
+        font-size: 14px;
+        &:hover {
+          background: rgba(0, 0, 0, 0.08);
+        }
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px;
+        }
+        span {
+          color: #70b5f9;
+          margin-top: 2px;
+        }
+      }
+    }
+  }
+`;
+
+const Content = styled.div``;
 
 export default Main;
